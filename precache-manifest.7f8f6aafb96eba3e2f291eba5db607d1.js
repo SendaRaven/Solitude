d2000e418c39883dd02b8c68d7e9d1f42859dab8
SendaRaven <42833205+SendaRaven@users.noreply.github.com>
2019-10-08 00:56:21 +0200

    Updates

---
+++ precache-manifest.7f8f6aafb96eba3e2f291eba5db607d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7129f2f357941e4f1098eece185bbd76",
+    "url": "/index.html"
+  },
+  {
+    "revision": "25683802947f92dda206",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "d726c157fdf6d0c70f06",
+    "url": "/static/css/main.498d002a.chunk.css"
+  },
+  {
+    "revision": "25683802947f92dda206",
+    "url": "/static/js/2.83288bb7.chunk.js"
+  },
+  {
+    "revision": "d726c157fdf6d0c70f06",
+    "url": "/static/js/main.2c3b783a.chunk.js"
+  },
+  {
+    "revision": "436801156fd632cbe932",
+    "url": "/static/js/runtime~main.346da20d.js"
+  }
+]);
